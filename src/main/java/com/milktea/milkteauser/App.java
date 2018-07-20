14f936c864529b23c98c01d73ccb64b90e37b169
caoxingyu <caoxingyu2016@qq.com>
2018-07-20 13:53:00 +0800

    first init

---
+++ src/main/java/com/milktea/milkteauser/App.java
@@ -0,0 +1,11 @@
+package com.milktea.milkteauser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
