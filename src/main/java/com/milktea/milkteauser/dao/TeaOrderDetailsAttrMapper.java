47fbc91eb419f1f4e92532fa6457a090f3c85fdd
caoxingyu <caoxingyu2016@qq.com>
2018-07-27 13:35:00 +0800

    划分moudle

--- src/main/java/com/milktea/milkteauser/dao/TeaOrderDetailsAttrMapper.java
+++
@@ -1,9 +0,0 @@
-package com.milktea.milkteauser.dao;
-
-import com.milktea.milkteauser.domain.TeaOrderDetailsAttr;
-
-public interface TeaOrderDetailsAttrMapper {
-    int insert(TeaOrderDetailsAttr record);
-
-    int insertSelective(TeaOrderDetailsAttr record);
-}
