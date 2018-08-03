c48a0fa9d247e7cf47e5f8be1964ef328219e415
caoxingyu <caoxingyu2016@qq.com>
2018-08-03 09:23:35 +0800

    微信支付测试实例

---
+++ milktea-user/src/main/java/com/milktea/milkteauser/Application.java
@@ -0,0 +1,13 @@
+package com.milktea.milkteauser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
