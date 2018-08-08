bb53cfa81d74a9e96060f1698d82f7b9ca85a953
Administrator <Administrator@iZy729e50bixvdZ>
2018-08-08 16:25:47 +0800

    COMMIT HTTP POST

--- milktea-user/src/main/java/com/milktea/milkteauser/controller/UserLoginController.java
+++ milktea-user/src/main/java/com/milktea/milkteauser/controller/UserLoginController.java
@@ -1,11 +1,12 @@
 package com.milktea.milkteauser.controller;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -14,6 +15,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSON;
@@ -23,6 +25,8 @@ import com.milktea.milkteauser.domain.TeaLoginWeixin;
 import com.milktea.milkteauser.exception.MilkTeaErrorConstant;
 import com.milktea.milkteauser.exception.MilkTeaException;
 import com.milktea.milkteauser.service.UserLoginService;
+import com.milktea.milkteauser.util.HttpUtil;
+import com.milktea.milkteauser.vo.ClassGoodsRequestVo;
 import com.milktea.milkteauser.vo.ResponseBody;
 
 
@@ -176,7 +180,7 @@ public class UserLoginController {
 	}
 	
 	//得到所有店铺LIST
-	@RequestMapping(value="/storelist", method = RequestMethod.POST)
+	@RequestMapping(value="/storelist", method = RequestMethod.GET)
 	public ResponseBody<JSONObject>  getStoreList() throws MilkTeaException{
 		BufferedReader in = null;
 		String result = "";
@@ -187,7 +191,7 @@ public class UserLoginController {
         
         //所有商铺 
 //		param = "appid=" + weiXinAppid +"&" + "secret=" + weiXinSecret + "&" + "code=" + code + "&" + "grant_type=authorization_code";
-        String url = "https://******/queryStores";
+        String url = "http://localhost:8081/queryStores";
         String urlNameString = url ;
         
         try {
@@ -214,6 +218,7 @@ public class UserLoginController {
 	        while ((line = in.readLine()) != null) {
 	            result += line;
 	        }
+	        System.out.println(result);
 	        jsonObject = JSON.parseObject(result);
 	        responseBody.setData(jsonObject);
         }  catch (Exception e) {
@@ -234,117 +239,44 @@ public class UserLoginController {
 	}
 	
 	//取得店铺内的商品
-	@RequestMapping(value="/getClassGoods", method = RequestMethod.POST)
-	public ResponseBody<JSONObject>  getClassGoods(String storeNo,String classType) throws MilkTeaException{
+	@RequestMapping(value="/getClassGoods")
+	public ResponseBody<JSONObject>  getClassGoods(@RequestParam("storeNo") String storeNo,@RequestParam("classType") String classType) throws MilkTeaException{
 		BufferedReader in = null;
 		String result = "";
 		Logger logger = LoggerFactory.getLogger(UserLoginController.class);
 		ResponseBody<JSONObject> responseBody = new ResponseBody<JSONObject>();
 		JSONObject jsonObject = new JSONObject();
+		JsonObject message = new JsonObject();
+		PrintWriter out = null;
+		String path = "http://localhost:8081/queryClassGoods"; 
+		message.addProperty("storeNo", storeNo);
+		message.addProperty("classType", classType);
 		
-		//调用商品后台，取得默认登入商铺内的商品及所有商铺
+		//将对像转成json格式
+		ClassGoodsRequestVo ClassGoodsRequestVo = new ClassGoodsRequestVo();
+		ClassGoodsRequestVo.setStoreNo(storeNo);
+		ClassGoodsRequestVo.setClassType(classType);
+		String json = JSON.toJSONString(ClassGoodsRequestVo);
         
-        //所有商铺 
-//			param = "appid=" + weiXinAppid +"&" + "secret=" + weiXinSecret + "&" + "code=" + code + "&" + "grant_type=authorization_code";
-        String url = "https://******/queryClassGoods";
-//        String urlNameString = url + "?" + JSON.toJSONString(classGoodsRequestVo);
-        
-//        try {
-//	        URL realUrl = new URL(urlNameString);
-//	        // 打开和URL之间的连接
-//	        URLConnection connection = realUrl.openConnection();
-//	        // 设置通用的请求属性
-//	        connection.setRequestProperty("accept", "*/*");
-//	        connection.setRequestProperty("connection", "Keep-Alive");
-//	        connection.setRequestProperty("user-agent",
-//	                "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-//	        // 建立实际的连接
-//	        connection.connect();
-//	        // 获取所有响应头字段
-//	        Map<String, List<String>> map = connection.getHeaderFields();
-//	        // 遍历所有的响应头字段
-//	        for (String key : map.keySet()) {
-//	            System.out.println(key + "--->" + map.get(key));
-//	        }
-//	        // 定义 BufferedReader输入流来读取URL的响应
-//	        in = new BufferedReader(new InputStreamReader(
-//	                connection.getInputStream()));
-//	        String line = "";
-//	        while ((line = in.readLine()) != null) {
-//	            result += line;
-//	        }
-//	        jsonObject = JSON.parseObject(result);
-//	        responseBody.setData(jsonObject);
-//        }  catch (Exception e) {
-//        	logger.error(MilkTeaErrorConstant.MILETEA_SHOP_FAILURE.getCnErrorMsg(), e);
-//            throw new MilkTeaException(MilkTeaErrorConstant.MILETEA_SHOP_FAILURE, e);
-//        }
-//        // 使用finally块来关闭输入流
-//        finally {
-//            try {
-//                if (in != null) { }
-//            } catch (Exception e2) {
-//                e2.printStackTrace();
-//            }
-//        }
-        //POST 请求
-        JsonObject message = new JsonObject();
-        message.addProperty("storeNo", storeNo);
-        message.addProperty("classType", classType);
-        PrintWriter out = null;
-//        InputStream in = null;
-        JsonObject jo = null;
-        try {
-            // 打开和URL之间的连接
-            URLConnection conn = new URL(url).openConnection();
-            // 设置通用的请求属性
-            conn.setRequestProperty("accept", "*/*");
-            conn.setRequestProperty("connection", "Keep-Alive");
-            conn.setRequestProperty("user-agent",
-                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-            // 发送POST请求必须设置如下两行
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
-            // 获取URLConnection对象对应的输出流
-            out = new PrintWriter(conn.getOutputStream());
-            // 发送请求参数
-            out.print(message.toString());
-            // flush输出流的缓冲
-            out.flush();
-            // POST请求
-     
-            out.flush();
-            out.close();
-	        // 定义 BufferedReader输入流来读取URL的响应
-	        in = new BufferedReader(new InputStreamReader(
-	        		conn.getInputStream()));
-	        String line = "";
-	        while ((line = in.readLine()) != null) {
-	            result += line;
-	        }
-	        jsonObject = JSON.parseObject(result);
+        //1, 得到URL对象  
+        URL url;
+		try {
+			url = new URL(path);
+			HttpUtil HttpUtil = new HttpUtil();
+			Map<String,String> mapParam = new HashMap<String,String>();
+			mapParam.put("storeNo", storeNo);
+			mapParam.put("classType", classType);
+			String retStr = HttpUtil.post(path, mapParam);
+			System.out.println(retStr);
+			jsonObject = JSON.parseObject(retStr);
 	        responseBody.setData(jsonObject);
-
-        }  catch (Exception e) {
-        	logger.error(MilkTeaErrorConstant.MILETEA_SHOP_FAILURE.getCnErrorMsg(), e);
-            throw new MilkTeaException(MilkTeaErrorConstant.MILETEA_SHOP_FAILURE, e);
-//        }
-        } finally {
-            if (out != null) {
-                out.flush();
-                out.close();
-            }
-            if (in != null ){
-                try {
-                    in.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        
-        
-        
+			
+		
+          
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
         
 		return responseBody;
 	}
