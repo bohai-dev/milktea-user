eab7e073c2654b841ad6de403f77a4db3ba5613b
caoxingyu <1746569077@qq.com>
2018-08-11 15:39:06 +0800

    HttpUtil更新

--- milktea-user/src/main/java/com/milktea/milkteauser/util/HttpUtil.java
+++ milktea-user/src/main/java/com/milktea/milkteauser/util/HttpUtil.java
@@ -25,14 +25,19 @@ public class HttpUtil {
 
 
 
-    public static  String get(String url) throws IOException{
+    public static  String get(String url) throws Exception{
         CloseableHttpClient httpClient=HttpClients.createDefault(); // 创建httpClient实例
         HttpGet httpGet=new HttpGet();
         httpGet.setURI(URI.create(url));
        // httpGet.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko"); // 设置请求头消息User-Agent
-        CloseableHttpResponse response=httpClient.execute(httpGet); // 执行http get请求
-        HttpEntity entity=response.getEntity(); // 获取返回实体
+        CloseableHttpResponse response= null; // 执行http get请求
+        response = httpClient.execute(httpGet);
+        int statusCode=response.getStatusLine().getStatusCode();
+        if (statusCode!=200){
+           throw new Exception("无法连接该地址");
+        }
 
+        HttpEntity entity=response.getEntity(); // 获取返回实体
         String content=EntityUtils.toString(entity,"utf-8");
         response.close(); // response关闭
         httpClient.close(); // httpClient关闭httpClient关闭
@@ -46,7 +51,7 @@ public class HttpUtil {
      * @return    返回内容
      * @throws IOException
      */
-    public static String get(String url, Map<String,String> params) throws IOException{
+    public static String get(String url, Map<String,String> params) throws Exception{
         StringBuilder href=new StringBuilder(url+"?");
         params.forEach((k,v)->{
               href.append(k).append("=").append(v).append("&");
@@ -56,6 +61,10 @@ public class HttpUtil {
         HttpGet httpGet=new HttpGet(href.toString());
         // httpGet.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko"); // 设置请求头消息User-Agent
         CloseableHttpResponse response=httpClient.execute(httpGet); // 执行http get请求
+        int statusCode=response.getStatusLine().getStatusCode();
+        if (statusCode!=200){
+            throw new Exception("无法连接该地址");
+        }
         HttpEntity entity=response.getEntity(); // 获取返回实体
         String content=EntityUtils.toString(entity,"utf-8");
         response.close();
@@ -69,13 +78,17 @@ public class HttpUtil {
      * @return
      * @throws IOException
      */
-     public static String post(String url) throws IOException{
+     public static String post(String url) throws Exception{
          CloseableHttpClient client = HttpClients.createDefault();
          HttpPost httpPost = new HttpPost(url);
          httpPost.setHeader("Accept", "application/json");
          httpPost.setHeader("Content-type", "application/json");
 
          CloseableHttpResponse response = client.execute(httpPost);
+         int statusCode=response.getStatusLine().getStatusCode();
+         if (statusCode!=200){
+             throw new Exception("无法连接该地址");
+         }
          HttpEntity resEntity=response.getEntity(); // 获取返回实体
          String content=EntityUtils.toString(resEntity,"utf-8");
          //  assertThat(response.getStatusLine().getStatusCode(), equalTo(200));
@@ -92,7 +105,7 @@ public class HttpUtil {
      * @return
      * @throws IOException
      */
-    public static String post(String url,Map<String,String> params) throws IOException{
+    public static String post(String url,Map<String,String> params) throws Exception{
         CloseableHttpClient client = HttpClients.createDefault();
         HttpPost httpPost = new HttpPost(url);
         //将Map转为Json
@@ -104,6 +117,10 @@ public class HttpUtil {
         httpPost.setHeader("Content-type", "application/json");
 
         CloseableHttpResponse response = client.execute(httpPost);
+        int statusCode=response.getStatusLine().getStatusCode();
+        if (statusCode!=200){
+            throw new Exception("无法连接该地址");
+         }
         HttpEntity resEntity=response.getEntity(); // 获取返回实体
         String content=EntityUtils.toString(resEntity,"utf-8");
       //  assertThat(response.getStatusLine().getStatusCode(), equalTo(200));
