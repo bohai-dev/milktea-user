f2f5af98f08fb4fc31bc5b217e99dd076e63dcad
caoding599@sohu.com <cao830107>
2018-08-20 16:44:28 +0800

    客户订单查询装载

---
+++ milktea-user/src/main/java/com/milktea/milkteauser/vo/MessageType.java
@@ -0,0 +1,17 @@
+package com.milktea.milkteauser.vo;
+
+public class MessageType {
+    
+    //消息类型 0：历史订单 1：新订单 2：更新订单 3：订单完成  4：半小时提醒 5：撤单  
+    private String messageType;
+
+    public String getMessageType() {
+        return messageType;
+    }
+
+    public void setMessageType(String messageType) {
+        this.messageType = messageType;
+    }
+
+    
+}
