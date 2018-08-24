8227acf1742607e6c1ee33b09b43877de97ee1d7
caoding599@sohu.com <cao830107>
2018-08-24 13:48:55 +0800

    客户分页查询订单信息

--- milktea-user/src/main/java/com/milktea/milkteauser/controller/UserOrderInfoController.java
+++ milktea-user/src/main/java/com/milktea/milkteauser/controller/UserOrderInfoController.java
@@ -6,6 +6,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,12 +24,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.milktea.milkteauser.exception.MilkTeaException;
 import com.milktea.milkteauser.service.UserOrderInfoService;
 import com.milktea.milkteauser.util.HttpUtil;
 import com.milktea.milkteauser.vo.CustOrderInfoVo;
-import com.milktea.milkteauser.vo.OrderNationVo;
+import com.milktea.milkteauser.vo.PageRequestVo;
 import com.milktea.milkteauser.vo.QueryOrdersRequestVo;
 import com.milktea.milkteauser.vo.ResponseBody;
 import com.milktea.milkteauser.vo.ResponseHeader;
@@ -89,7 +97,81 @@ public class UserOrderInfoController {
 		return responseHeader;
 	}
 	
-	
+	/**
+	 * 根据客户号分页条件查询订单
+	 * @param requestVo
+	 * @return
+	 * @throws MilkTeaException
+	 */
+	@RequestMapping(value="/queryOrdersByUserNoPage", method = RequestMethod.POST)
+	public ResponseBody<JSONObject>  queryOrdersByUserNoPage(@RequestBody PageRequestVo<QueryOrdersRequestVo> requestVo) throws MilkTeaException{
+		BufferedReader in = null;
+		String result = "";
+		Logger logger = LoggerFactory.getLogger(UserLoginController.class);
+		ResponseBody<JSONObject> responseBody = new ResponseBody<JSONObject>();
+		JSONObject jsonObject = new JSONObject();
+		JsonObject message = new JsonObject();
+		PrintWriter out = null;
+		String path = "http://localhost:8081/queryOrdersByUserNo";
+	        
+		try {
+
+			HttpUtil HttpUtil = new HttpUtil();
+			Map<String,String> mapParam = new HashMap<String,String>();
+
+//			mapParam.put("userNo", requestVo.getUserNo());
+//			mapParam.put("telephone", requestVo.getTelephone());
+//			mapParam.put("orderNo", requestVo.getOrderNo());
+//			mapParam.put("lang", requestVo.getLang());
+//			mapParam.put("storeNo", requestVo.getStoreNo());
+//			mapParam.put("promotionId", requestVo.getPromotionId());
+//			mapParam.put("orderType", requestVo.getOrderType());
+//			mapParam.put("orderStatus", requestVo.getOrderStatus());
+//			mapParam.put("payStatus", requestVo.getPayStatus());
+//			if(null != requestVo.getBeginDate())
+//			{
+//				mapParam.put("beginDate", requestVo.getBeginDate().toString());
+//			}
+//			if(null != requestVo.getEndDate())
+//			{
+//				mapParam.put("endDate", requestVo.getEndDate().toString());
+//			}
+			CloseableHttpClient client = HttpClients.createDefault();
+	        HttpPost httpPost = new HttpPost(path);
+	        //将Map转为Json
+	        Gson gson = new Gson();
+	        String json = gson.toJson(requestVo);
+	        StringEntity entity = new StringEntity(json);
+	        httpPost.setEntity(entity);
+	        httpPost.setHeader("Accept", "application/json");
+	        httpPost.setHeader("Content-type", "application/json");
+
+	        CloseableHttpResponse response = client.execute(httpPost);
+	        int statusCode=response.getStatusLine().getStatusCode();
+	        if (statusCode!=200){
+	            throw new Exception("无法连接该地址");
+	         }
+	        HttpEntity resEntity=response.getEntity(); // 获取返回实体
+	        String content=EntityUtils.toString(resEntity,"utf-8");
+	      //  assertThat(response.getStatusLine().getStatusCode(), equalTo(200));
+	        response.close();
+	        client.close();
+			
+			
+			System.out.println(content);
+			jsonObject = JSON.parseObject(content);
+	        responseBody.setData(jsonObject);
+		
+          
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+		 
+		 
+		 
+		return responseBody;
+	}
 
 	/**
 	 * 根据客户号条件查询订单
