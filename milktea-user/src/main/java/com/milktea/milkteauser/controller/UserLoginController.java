2ba548ec043feb156673f42e502ed59df6deba12
caoding599@sohu.com <cao830107>
2018-09-03 10:02:00 +0800

    因为乱码 GET方法重写

--- milktea-user/src/main/java/com/milktea/milkteauser/controller/UserLoginController.java
+++ milktea-user/src/main/java/com/milktea/milkteauser/controller/UserLoginController.java
@@ -95,56 +95,74 @@ public class UserLoginController {
         
         //所有商铺 
 //		param = "appid=" + weiXinAppid +"&" + "secret=" + weiXinSecret + "&" + "code=" + code + "&" + "grant_type=authorization_code";
-        String url = "http://localhost:8081/queryStores";
-        String urlNameString = "" ;
-        if("".equals(lang)){
-        	 urlNameString = url ;
-        } else {
-        	urlNameString = url + "/" +lang;
-        }
-        
-        
+//        String url = "http://localhost:8081/queryStores";
+//        String urlNameString = "" ;
+//        if("".equals(lang)){
+//        	 urlNameString = url ;
+//        } else {
+//        	urlNameString = url + "/" +lang;
+//        }
+		String path = "http://localhost:8081/queryStores";
         try {
-	        URL realUrl = new URL(urlNameString);
-	        // 打开和URL之间的连接
-	        URLConnection connection = realUrl.openConnection();
-	        // 设置通用的请求属性
-	        connection.setRequestProperty("accept", "*/*");
-	        connection.setRequestProperty("connection", "Keep-Alive");
-	        connection.setRequestProperty("user-agent",
-	                "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-	        // 建立实际的连接
-	        connection.connect();
-	        // 获取所有响应头字段
-	        Map<String, List<String>> map = connection.getHeaderFields();
-	        // 遍历所有的响应头字段
-	        for (String key : map.keySet()) {
-	            System.out.println(key + "--->" + map.get(key));
-	        }
-	        // 定义 BufferedReader输入流来读取URL的响应
-	        in = new BufferedReader(new InputStreamReader(
-	                connection.getInputStream()));
-	        String line = "";
-	        while ((line = in.readLine()) != null) {
-	            result += line;
-	        }
-	        System.out.println(result);
-	        jsonObject = JSON.parseObject(result);
+
+			HttpUtil HttpUtil = new HttpUtil();
+			Map<String,String> mapParam = new HashMap<String,String>();
+			mapParam.put("lang", lang);
+
+			String retStr = HttpUtil.get(path, mapParam);
+			System.out.println(retStr);
+			jsonObject = JSON.parseObject(retStr);
 	        responseBody.setData(jsonObject);
-        }  catch (Exception e) {
-        	logger.error(MilkTeaErrorConstant.MILETEA_SHOP_FAILURE.getCnErrorMsg(), e);
-            throw new MilkTeaException(MilkTeaErrorConstant.MILETEA_SHOP_FAILURE, e);
-        }
-        // 使用finally块来关闭输入流
-        finally {
-            try {
-                if (in != null) {
-                    in.close();
-                }
-            } catch (Exception e2) {
-                e2.printStackTrace();
-            }
-        }
+			
+		
+          
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+        
+        
+//        try {
+//	        URL realUrl = new URL(urlNameString);
+//	        // 打开和URL之间的连接
+//	        URLConnection connection = realUrl.openConnection();
+//	        // 设置通用的请求属性
+//	        connection.setRequestProperty("accept", "*/*");
+//	        connection.setRequestProperty("connection", "Keep-Alive");
+//	        connection.setRequestProperty("user-agent",
+//	                "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+//	        // 建立实际的连接
+//	        connection.connect();
+//	        // 获取所有响应头字段
+//	        Map<String, List<String>> map = connection.getHeaderFields();
+//	        // 遍历所有的响应头字段
+//	        for (String key : map.keySet()) {
+//	            System.out.println(key + "--->" + map.get(key));
+//	        }
+//	        // 定义 BufferedReader输入流来读取URL的响应
+//	        in = new BufferedReader(new InputStreamReader(
+//	                connection.getInputStream()));
+//	        String line = "";
+//	        while ((line = in.readLine()) != null) {
+//	            result += line;
+//	        }
+//	        System.out.println(result);
+//	        jsonObject = JSON.parseObject(result);
+//	        responseBody.setData(jsonObject);
+//        }  catch (Exception e) {
+//        	logger.error(MilkTeaErrorConstant.MILETEA_SHOP_FAILURE.getCnErrorMsg(), e);
+//            throw new MilkTeaException(MilkTeaErrorConstant.MILETEA_SHOP_FAILURE, e);
+//        }
+//        // 使用finally块来关闭输入流
+//        finally {
+//            try {
+//                if (in != null) {
+//                    in.close();
+//                }
+//            } catch (Exception e2) {
+//                e2.printStackTrace();
+//            }
+//        }
 		return responseBody;
 	}
 	
