153b01ee57f7c6a70ee7e66bf017cd3cfc018243
caoxingyu <caoxingyu2016@qq.com>
2018-09-25 13:14:16 +0800

    修改IOTPay

--- milktea-user/src/main/java/com/milktea/milkteauser/service/PayInfoService.java
+++ milktea-user/src/main/java/com/milktea/milkteauser/service/PayInfoService.java
@@ -2,6 +2,7 @@ package com.milktea.milkteauser.service;
 
 import com.milktea.milkteauser.exception.MilkTeaException;
 import com.milktea.milkteauser.vo.IOTBean;
+import com.milktea.milkteauser.vo.IotResponseBean;
 import com.milktea.milkteauser.vo.ResponseBody;
 import com.milktea.milkteauser.vo.StripeBean;
 
@@ -9,4 +10,5 @@ public interface PayInfoService {
 
     void  stripePay(StripeBean stripeBean) throws MilkTeaException;
     ResponseBody<String> iotPay(IOTBean iotBean) throws MilkTeaException;
+    String iotNotify(IotResponseBean iotResponseBean) throws MilkTeaException;
 }
