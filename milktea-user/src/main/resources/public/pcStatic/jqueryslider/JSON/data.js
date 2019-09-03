954d2f8ae995cdae49b31dd0796cb5b1804c5dcd
Administrator <Administrator@iZy729e50bixvdZ>
2019-09-03 09:33:54 +0800

    合并

---
+++ milktea-user/src/main/resources/public/pcStatic/jqueryslider/JSON/data.js
@@ -0,0 +1,46 @@
+{	
+    "images":[
+        {
+            "title":"Cavaliers",
+            "des":"Cleveland",
+            "imgpath":"img/2.png"
+        },
+        {"title":"Bulls","des":"Chicago","imgpath":"img/1.png"},
+        {"title":"Warriors","des":"Golden State","imgpath":"img/3.png"},
+        {"title":"Heat","des":"Miami","imgpath":"img/4.png"},
+        {"title":"Thunder","des":"Oklahoma des","imgpath":"img/5.png"},
+        {"title":"Spurs","des":"San Antonio","imgpath":"img/6.png"},
+        {"title":"Rockets","des":"Houston","imgpath":"img/7.png"},
+        {"title":"Lakers","des":"Los Angeles","imgpath":"img/1.png"},
+        {"title":"Bucks","des":"Milwaukee","imgpath":"img/2.png"},
+        {"title":"Celtics","des":"Boston","imgpath":"img/3.png"},
+        {"title":"Hornets","des":"Charlotte","imgpath":"img/4.png"},
+        {"title":"Pistons","des":"Detroit","imgpath":"img/5.png"},
+        {"title":"Mavericks","des":"Dallas","imgpath":"img/6.png"},
+        {"title":"Hawks","des":"Atlanta","imgpath":"img/7.png"},
+        {"title":"Grizzlies","des":"Memphis","imgpath":"img/1.png"},
+        {"title":"Raptors","des":"Toronto","imgpath":"img/2.png"}
+        ],
+    "Movie":[
+        {
+            "title":"WaltDisney",
+            "des":"???",
+            "imgpath":"img/5.png"
+        },
+        {"title":"Bulls","des":"Chicago","imgpath":"img/1.png"},
+        {"title":"Warriors","des":"Golden State","imgpath":"img/3.png"},
+        {"title":"Heat","des":"Miami","imgpath":"img/4.png"},
+        {"title":"Thunder","des":"Oklahoma des","imgpath":"img/5.png"},
+        {"title":"Spurs","des":"San Antonio","imgpath":"img/6.png"},
+        {"title":"Rockets","des":"Houston","imgpath":"img/7.png"},
+        {"title":"Lakers","des":"Los Angeles","imgpath":"img/1.png"},
+        {"title":"Bucks","des":"Milwaukee","imgpath":"img/2.png"},
+        {"title":"Celtics","des":"Boston","imgpath":"img/3.png"},
+        {"title":"Hornets","des":"Charlotte","imgpath":"img/4.png"},
+        {"title":"Pistons","des":"Detroit","imgpath":"img/5.png"},
+        {"title":"Mavericks","des":"Dallas","imgpath":"img/6.png"},
+        {"title":"Hawks","des":"Atlanta","imgpath":"img/7.png"},
+        {"title":"Grizzlies","des":"Memphis","imgpath":"img/1.png"},
+        {"title":"Raptors","des":"Toronto","imgpath":"img/2.png"}
+        ]
+}
